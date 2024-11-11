5a53c93805f5a28be012e0a1b374fa527e62a704
Hubber773 <githubber53@gmail.com>
2024-11-11 16:51:57 +0100

    Pasek postępu i inne poprawki takie jak kolory

--- KolejneZadanie/MainWindow.xaml.cs
+++ KolejneZadanie/MainWindow.xaml.cs
@@ -13,6 +13,20 @@ namespace KolejneZadanie
 {
     public partial class MainWindow : Window
     {
+        private bool isPogrubienieUsed = false;
+        private bool isKursywaUsed = false;
+        private bool isPodkreslenieUsed = false;
+        private bool isCzerwonyUsed = false;
+        private bool isZielonyUsed = false;
+        private bool isNiebieskiUsed = false;
+        private bool isArialUsed = false;
+        private bool isCalibriUsed = false;
+        private bool isTimesNewRomanUsed = false;
+        private bool isRozmiarUsed = false;
+        private bool isLeftAlignUsed = false;
+        private bool isCenterAlignUsed = false;
+        private bool isRightAlignUsed = false;
+        private bool isJustifyUsed = false;
 
         public MainWindow()
         {
@@ -22,16 +36,31 @@ namespace KolejneZadanie
         private void ChbPogrubienie_Checked(object sender, RoutedEventArgs e)
         {
             txtTekst.FontWeight = ChbPogrubienie.IsChecked == true ? FontWeights.Bold : FontWeights.Normal;
+            if (!isPogrubienieUsed)
+            {
+                isPogrubienieUsed = true;
+                UpdateProgress();
+            }
         }
 
         private void ChbKursywa_Checked(object sender, RoutedEventArgs e)
         {
             txtTekst.FontStyle = ChbKursywa.IsChecked == true ? FontStyles.Italic : FontStyles.Normal;
+            if (!isKursywaUsed)
+            {
+                isKursywaUsed = true;
+                UpdateProgress();
+            }
         }
 
         private void ChbPodkreslenie_Checked(object sender, RoutedEventArgs e)
         {
             txtTekst.TextDecorations = ChbPodkreslenie.IsChecked == true ? TextDecorations.Underline : null;
+            if (!isPodkreslenieUsed)
+            {
+                isPodkreslenieUsed = true;
+                UpdateProgress();
+            }
         }
 
         private void CzerwonyKolor_Checked(object sender, RoutedEventArgs e)
@@ -39,6 +68,11 @@ namespace KolejneZadanie
             if (CzerwonyKolor.IsChecked == true)
             {
                 txtTekst.Foreground = Brushes.Red;
+                if (!isCzerwonyUsed)
+                {
+                    isCzerwonyUsed = true;
+                    UpdateProgress();
+                }
             }
         }
 
@@ -47,6 +81,11 @@ namespace KolejneZadanie
             if (ZielonyKolor.IsChecked == true)
             {
                 txtTekst.Foreground = Brushes.Green;
+                if (!isZielonyUsed)
+                {
+                    isZielonyUsed = true;
+                    UpdateProgress();
+                }
             }
         }
 
@@ -55,6 +94,11 @@ namespace KolejneZadanie
             if (NiebieskiKolor.IsChecked == true)
             {
                 txtTekst.Foreground = Brushes.Blue;
+                if (!isNiebieskiUsed)
+                {
+                    isNiebieskiUsed = true;
+                    UpdateProgress();
+                }
             }
         }
 
@@ -63,6 +107,11 @@ namespace KolejneZadanie
             if (ArialCzcionka.IsChecked == true)
             {
                 txtTekst.FontFamily = new FontFamily("Arial");
+                if (!isArialUsed)
+                {
+                    isArialUsed = true;
+                    UpdateProgress();
+                }
             }
         }
 
@@ -71,6 +120,11 @@ namespace KolejneZadanie
             if (CalibriCzcionka.IsChecked == true)
             {
                 txtTekst.FontFamily = new FontFamily("Calibri");
+                if (!isCalibriUsed)
+                {
+                    isCalibriUsed = true;
+                    UpdateProgress();
+                }
             }
         }
 
@@ -79,32 +133,86 @@ namespace KolejneZadanie
             if (TimesNewRomanCzcionka.IsChecked == true)
             {
                 txtTekst.FontFamily = new FontFamily("Times New Roman");
+                if (!isTimesNewRomanUsed)
+                {
+                    isTimesNewRomanUsed = true;
+                    UpdateProgress();
+                }
             }
         }
 
         private void SuwakRozmiarCzcionki_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             txtTekst.FontSize = SuwakRozmiarCzcionki.Value;
+            if (!isRozmiarUsed && SuwakRozmiarCzcionki.Value > SuwakRozmiarCzcionki.Minimum)
+            {
+                isRozmiarUsed = true;
+                UpdateProgress();
+            }
         }
 
         private void WyrownajDoLewej_Click(object sender, RoutedEventArgs e)
         {
             txtTekst.TextAlignment = TextAlignment.Left;
+            if (!isLeftAlignUsed)
+            {
+                isLeftAlignUsed = true;
+                UpdateProgress();
+            }
         }
 
         private void WyrownajDoSrodka_Click(object sender, RoutedEventArgs e)
         {
             txtTekst.TextAlignment = TextAlignment.Center;
+            if (!isCenterAlignUsed)
+            {
+                isCenterAlignUsed = true;
+                UpdateProgress();
+            }
         }
 
         private void WyrownajDoPrawej_Click(object sender, RoutedEventArgs e)
         {
             txtTekst.TextAlignment = TextAlignment.Right;
+            if (!isRightAlignUsed)
+            {
+                isRightAlignUsed = true;
+                UpdateProgress();
+            }
         }
 
         private void Wyrownaj_Click(object sender, RoutedEventArgs e)
         {
             txtTekst.TextAlignment = TextAlignment.Justify;
+            if (!isJustifyUsed)
+            {
+                isJustifyUsed = true;
+                UpdateProgress();
+            }
+        }
+
+        private void UpdateProgress() 
+        {
+            if (PasekPostepu != null)
+            {
+                int count = 0;
+                if (isPogrubienieUsed) count++;
+                if (isKursywaUsed) count++;
+                if (isPodkreslenieUsed) count++;
+                if (isCzerwonyUsed) count++;
+                if (isZielonyUsed) count++;
+                if (isNiebieskiUsed) count++;
+                if (isArialUsed) count++;
+                if (isCalibriUsed) count++;
+                if (isTimesNewRomanUsed) count++;
+                if (isRozmiarUsed) count++;
+                if (isLeftAlignUsed) count++;
+                if (isCenterAlignUsed) count++;
+                if (isRightAlignUsed) count++;
+                if (isJustifyUsed) count++;
+
+                PasekPostepu.Value = (double)count / 14 * 100;
+            }
         }
     }
 }
